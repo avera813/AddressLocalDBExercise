23341fd1d1f1fb3061051f7e2e5396ee0e5ce758
Alex Vera <v-alexve@microsoft.com>
2016-11-01 14:25:44 -0700

    Adding DB.

--- AddressLocalDB/MainWindow.xaml.cs
+++ AddressLocalDB/MainWindow.xaml.cs
@@ -20,9 +20,29 @@ namespace AddressLocalDB
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private AddressDBDataSet addressDBDataSet;
+        private AddressDBDataSetTableAdapters.AddressTableAdapter addressDBDataSetAddressTableAdapter;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            addressDBDataSet = ((AddressLocalDB.AddressDBDataSet)(this.FindResource("addressDBDataSet")));
+            // Load data into the table Address. You can modify this code as needed.
+            addressDBDataSetAddressTableAdapter = new AddressLocalDB.AddressDBDataSetTableAdapters.AddressTableAdapter();
+            addressDBDataSetAddressTableAdapter.Fill(addressDBDataSet.Address);
+            System.Windows.Data.CollectionViewSource addressViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("addressViewSource")));
+            addressViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            addressDBDataSetAddressTableAdapter.Update(addressDBDataSet.Address);
         }
     }
 }
